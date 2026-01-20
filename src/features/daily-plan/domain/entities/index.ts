07b12ffb3e130b2fc4024e4c3df34827e582a82c
Ruggeri <crfrudin@gmail.com>
2026-01-20 16:11:56 -0300

    fix(daily-plan): add entities barrel export for domain imports

---
+++ src/features/daily-plan/domain/entities/index.ts
@@ -0,0 +1,6 @@
+// src/features/daily-plan/domain/entities/index.ts
+export * from './CalendarProjection';
+export * from './DailyPlan';
+export * from './DailyPlanItem';
+export * from './ExecutedDay';
+export * from './PlannedTask';
