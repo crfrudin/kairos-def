6f107509344d446faba7ef49dd32e2af4beb4a69
Ruggeri <crfrudin@gmail.com>
2026-01-20 16:10:57 -0300

    fix(daily-plan): add value-objects barrel export for domain imports

---
+++ src/features/daily-plan/domain/value-objects/index.ts
@@ -0,0 +1,6 @@
+// src/features/daily-plan/domain/value-objects/index.ts
+export * from './CalendarDate';
+export * from './PlannedDuration';
+export * from './PlanStatus';
+export * from './TaskType';
+export * from './TimeBlock';
